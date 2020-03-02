5e85b44357984d339f3c5c1abc0f43cb1a7f4865
Kai Widman <Mark@brandnewbook.local>
2020-03-02 14:16:34 -0600

    Quick comment to test github

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+	# Great stuff.
     belongs_to :subject
     validates_presence_of :title
     validates_numericality_of :price, :message=>"Make sure price is a number"
