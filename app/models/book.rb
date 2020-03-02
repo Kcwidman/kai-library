4fe0e44da91ef0df192e9b456a2796030772f406
Kai Widman <kcwidman@gmail.com>
2020-03-02 15:07:44 -0600

    Learning how to use gitgub: removing test comment

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,4 @@
 class Book < ApplicationRecord
-	# Great stuff 2!
     belongs_to :subject
     validates_presence_of :title
     validates_numericality_of :price, :message=>"Make sure price is a number"
