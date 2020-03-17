55d2665a716b8e483ed27b64e2599273eed79da8
Kai Widman <kcwidman@gmail.com>
2020-03-17 12:05:36 -0500

    Solved spelling issue, added links to show page, default subject set to "Unknown"

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+<<<<<<< Updated upstream
   def index
     @books = Book.all
   end
@@ -59,4 +60,70 @@ class BooksController < ApplicationController
   def show_subjects
     @subject = Subject.find(params[:id])
   end
+=======
+    
+    def index
+        @books=Book.all
+    end
+
+    def show
+        @book=Book.find(params[:id])
+    end
+
+    def new
+        @book=Book.new
+        @subjects=Subject.all
+    end
+
+    def book_params
+        params.require(:books).permit(:title, :price, :subject_id, :description)
+    end
+  
+    def create
+        @book = Book.new(book_params)
+#Allow for empty subject, default to unknown
+        if @book.subject.nil?
+           @book.subject_id=6#default to Unkown subject if no subject is given
+        end
+        if @book.save
+           redirect_to books_path
+        else
+           @subjects = Subject.all
+           flash[:errors] = @book.errors.full_messages
+           render :action => 'new'
+        end
+    end
+
+    def edit
+        @book=Book.find(params[:id])
+        @subjects=Subject.all
+    end
+
+    def book_param
+        params.require(:book).permit(:title, :price, :subject_id, :description)
+    end
+
+    def update
+        @book=Book.find(params[:id])
+
+        if @book.update_attributes(book_param)
+            redirect_to :action => 'show', :id => @book
+        else 
+            @subjects=Subject.all
+            render :action => 'edit'
+        end
+
+    end
+
+    def destroy
+        Book.find(params[:id]).destroy
+        redirect_to :action => 'index'
+    end
+
+    def show_subjects
+        @subject = Subject.find(params[:id])
+    end
+
+
+>>>>>>> Stashed changes
 end
--- db/migrate/20200224213406_subjects.rb
+++ db/migrate/20200224213406_subjects.rb
@@ -4,11 +4,20 @@ class Subjects < ActiveRecord::Migration[6.0]
       t.column :name, :string
     end
 
+<<<<<<< Updated upstream
     Subject.create name: "Physics"
     Subject.create name: "Mathematics"
     Subject.create name: "Chemistry"
     Subject.create name: "Psychology"
     Subject.create name: "Geograpy"
+=======
+    Subject.create :name => "Physics"
+    Subject.create :name => "Mathematics"
+    Subject.create :name => "Chemistry"
+    Subject.create :name => "Psychology"
+    Subject.create :name => "Geography"
+    Subject.create :name => "Unknown"
+>>>>>>> Stashed changes
   end
 
   def self.down
