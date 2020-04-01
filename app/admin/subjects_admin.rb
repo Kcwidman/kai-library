29e3ee4cc2b31475b1f49a97678ccce9cfc89333
Kai Widman <kcwidman@gmail.com>
2020-04-01 10:56:47 -0500

    Trestle forms and index customized

--- app/admin/subjects_admin.rb
+++ app/admin/subjects_admin.rb
@@ -3,24 +3,18 @@ Trestle.resource(:subjects) do
     item :subjects, icon: "fa fa-star"
   end
 
-  controller do
-    def index
-      @subjects = Subject.all
-    end
-  end
   # Customize the table columns shown on the index view.
-  #
-  # table do
-  #   column :name
-  #   column :created_at, align: :center
-  #   actions
-  # end
+  
+  table do
+    column :name
+    actions
+  end
 
   # Customize the form fields shown on the new/edit views.
   #
   # form do |subject|
   #   text_field :name
-  #
+  
   #   row do
   #     col { datetime_field :updated_at }
   #     col { datetime_field :created_at }
