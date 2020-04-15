f2bfa2234eec92b5f86d73f3ed5616e2e8918d43
Kai Widman <kcwidman@gmail.com>
2020-04-15 11:17:31 -0500

    Using the Carrier Wave gem, books now have a cover. The books show page and admin page were updated to accommodate.

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   belongs_to :publisher, optional: true
   has_many :copies
   has_many :libraries, through: :copies
+  has_one_attached :cover
   validates_presence_of :title
   validates_numericality_of :price, allow_nil: true, message: "Make sure price is a number"
 
